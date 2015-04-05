6329a352d7226cfbfe659251540636a302d7385b
jdmarsh <jdmarsh2005@googlemail.com>
2015-04-05 15:30:07 +0100

    working with HTML

--- clipboard/clipboard/Clipboard.cpp
+++ clipboard/clipboard/Clipboard.cpp
@@ -1,26 +1,58 @@
 #include "Clipboard.h"
 
-std::string getClipboardText();
+std::string getClipboardHTML(UINT);
 std::string getClipboardFiles();
+std::string getClipboardText();
 
 std::string cb::getClipboardData() {
     std::string clipboardData;
     if (OpenClipboard(nullptr)) {
-        UINT formatList[2] = { CF_HDROP, CF_TEXT };
-        int format = GetPriorityClipboardFormat(formatList, 2);
-        if (format > 0) {
-            switch (format) {
-            case CF_TEXT:
-                clipboardData = getClipboardText();
-                break;
-            case CF_HDROP:
-                clipboardData = getClipboardFiles();
-                break;
-            }
+        UINT HTMLformat = RegisterClipboardFormat("HTML Format");
+        UINT formatList[3] = { HTMLformat, CF_HDROP, CF_TEXT };
+        int format = GetPriorityClipboardFormat(formatList, 3);
+        if (format == HTMLformat) {
+            clipboardData = getClipboardHTML(HTMLformat);
+        } else if (format == CF_HDROP) {
+            clipboardData = getClipboardFiles();
+        } else if (format == CF_TEXT) {
+            clipboardData = getClipboardText();
         }
     }
     CloseClipboard();
     return clipboardData;
+}
+
+std::string getClipboardHTML(UINT HTMLformat) {
+    std::string clipboardHTML;
+    HANDLE hData = GetClipboardData(HTMLformat);
+    if (hData != nullptr) {
+        char *hHTML = static_cast<char *>(GlobalLock(hData));
+        if (hHTML != nullptr) {
+            clipboardHTML.assign(hHTML);
+        }
+        GlobalUnlock(hData);
+    }
+    return clipboardHTML;
+}
+
+std::string getClipboardFiles() {
+    std::string clipboardFiles;
+    HANDLE hData = GetClipboardData(CF_HDROP);
+    if (hData != nullptr) {
+        HDROP hDropData = static_cast<HDROP>(GlobalLock(hData));
+        int fileCount = DragQueryFile(hDropData, -1, nullptr, 0);
+
+        for (int i = 0; i < fileCount; ++i) {
+            clipboardFiles.append(i == 0 ? "" : "\r\n");
+            int filenameSize = DragQueryFile(hDropData, i, NULL, 0);
+            char *filename = new char[filenameSize + 1];
+            DragQueryFile(hDropData, i, filename, filenameSize + 1);
+            clipboardFiles.append(filename);
+            delete[] filename;
+        }
+        GlobalUnlock(hData);
+    }
+    return clipboardFiles;
 }
 
 std::string getClipboardText() {
@@ -36,30 +68,10 @@ std::string getClipboardText() {
     return clipboardText;
 }
 
-std::string getClipboardFiles() {
-    std::string clipboardText;
-    HANDLE hData = GetClipboardData(CF_HDROP);
-    if (hData != nullptr) {
-        HDROP hDropData = static_cast<HDROP>(GlobalLock(hData));
-        int fileCount = DragQueryFile(hDropData, -1, nullptr, 0);
-
-        for (int i = 0; i < fileCount; ++i) {
-            clipboardText.append(i == 0 ? "" : "\r\n");
-            int filenameSize = DragQueryFile(hDropData, i, NULL, 0);
-            char *filename = new char[filenameSize + 1];
-            DragQueryFile(hDropData, i, filename, filenameSize + 1);
-            clipboardText.append(filename);
-            delete[] filename;
-        }
-        GlobalUnlock(hData);
-    }
-    return clipboardText;
-}
-
 void cb::setClipboardData(const std::string &text) {
     EmptyClipboard();
     HGLOBAL hClipboardMemory = GlobalAlloc(GMEM_MOVEABLE, text.size() + 1);
-    if (hClipboardMemory){
+    if (hClipboardMemory) {
         char *hClipboardData = static_cast<char *>(GlobalLock(hClipboardMemory));
         memcpy(hClipboardData, text.c_str(), text.size());
         GlobalUnlock(hClipboardMemory);
--- clipboard/clipboard/Clipboard.h
+++ clipboard/clipboard/Clipboard.h
@@ -1,8 +1,9 @@
 #include <string>
 #include <vector>
 #include <Windows.h>
+#include <iostream>
 
 namespace cb {
-	std::string getClipboardData();
-	void setClipboardData(const std::string &);
+    std::string getClipboardData();
+    void setClipboardData(const std::string &);
 }
